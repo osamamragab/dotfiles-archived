d96606b7734325c776fb694612e4dd23b15489e1
Osama <iosamaify@gmail.com>
2020-07-26 15:51:00 +0200

    setup: remove deleted files from files variable

--- setup.sh
+++ setup.sh
@@ -6,15 +6,11 @@ olddir="$HOME/dotfiles_old"
 
 # list of files/folders to symlink in homedir
 files=(
-  .zsh
-  .oh-my-zsh
   .vim
   .fonts
   .bashrc
-  .zshrc
   .vimrc
   .gitconfig
-  .npmrc
 )
 
 # create $olddir to backup existing dot files
