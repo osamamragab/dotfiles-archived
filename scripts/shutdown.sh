ca7842482721446a1fc1ac4db2cb5aa04d09a311
Osama <iosamaify@gmail.com>
2020-10-05 02:07:43 +0200

    add shutdown script

---
+++ scripts/shutdown.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+choice=$(echo "Shutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
+
+[ $choice = "Shutdown" ] && doas poweroff
+[ $choice = "Reboot" ] && doas reboot
+[ $choice = "Exit" ] && pkill dwm
+
+unset choice
