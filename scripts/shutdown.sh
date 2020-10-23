a474d59b8f2ea584b5d75436c580742a64e309e8
Osama <iosamaify@gmail.com>
2020-10-23 16:57:40 +0200

    scripts/shutdown: remove unset

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -5,5 +5,3 @@ choice=$(echo "Shutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
 [ $choice = "Shutdown" ] && doas poweroff
 [ $choice = "Reboot" ] && doas reboot
 [ $choice = "Exit" ] && pkill dwm
-
-unset choice
