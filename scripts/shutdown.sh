de54d5aedf77556e6790523d7bff2dd08784ca28
Osama <iosamaify@gmail.com>
2020-11-30 05:50:21 +0200

    scripts/volume, scripts/shutdown: add quotes

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-choice=$(echo "Shutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
+choice=$(printf "Shutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
 
-[ $choice = "Shutdown" ] && doas poweroff
-[ $choice = "Reboot" ] && doas reboot
-[ $choice = "Exit" ] && pkill dwm
+[ "$choice" = "Shutdown" ] && doas poweroff
+[ "$choice" = "Reboot" ] && doas reboot
+[ "$choice" = "Exit" ] && pkill dwm
