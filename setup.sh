a021a766d83a1214ca811709ba5f51f24ac6b1a3
Osama <iosamaify@gmail.com>
2020-12-16 11:47:23 +0200

    setup: improve statements

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ dotfiles_old_dir="$HOME/dotfiles_old"
 symlink_files=".xinitrc .xprofile .profile .inputrc .bashrc .bash_profile .vim .vimrc .gitconfig"
 
 # create needed directories
-[ ! -d "$HOME/bin" ] && mkdir "$HOME/bin"
-[ ! -d "$dotfiles_old_dir" ] && mkdir "$dotfiles_old_dir"
-[ ! -d "$dotfiles_old_dir/bin" ] && mkdir "$dotfiles_old_dir/bin"
+[ -d "$HOME/bin" ] || mkdir "$HOME/bin"
+[ -d "$dotfiles_old_dir" ] || mkdir "$dotfiles_old_dir"
+[ -d "$dotfiles_old_dir/bin" ] || mkdir "$dotfiles_old_dir/bin"
 
 # link $symlink_files to home directory
 for f in $symlink_files; do
@@ -23,8 +23,6 @@ for f in $symlink_files; do
 done
 
 for f in "$dotfiles_dir"/bin/*; do
-	[ -e "$f" ] || break
-
 	n=$(basename "$f")
 
 	if [ -e "$HOME/bin/$n" ]; then
