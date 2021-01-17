a5c951739d9acd780c3084597961617649c41e0c
Osama <iosamaify@gmail.com>
2021-01-17 05:37:16 +0200

    sysinit: add more packages

--- sysinit.sh
+++ sysinit.sh
@@ -16,7 +16,7 @@ echo "installing filesystems support..."
 doas xbps-install -Sy fuse fuse-exfat mtpfs simple-mtpfs autofs
 
 echo "installing build tools..."
-doas xbps-install -Sy git gcc clang make pkg-config
+doas xbps-install -Sy git gcc clang tcc make pkg-config
 
 echo "installing manual pages..."
 doas xbps-install -Sy man-pages man-pages-devel man-pages-posix tldr
@@ -25,22 +25,22 @@ echo "installing text editor..."
 doas xbps-install -Sy vim neovim
 
 echo "installing general tools..."
-doas xbps-install -Sy curl wget ssh gnupg2 pass sxhkd nnn maim youtube-dl rsync ripgrep bat xz groff upx jq glow translate-shell anki ctags
+doas xbps-install -Sy curl wget ssh gnupg2 pass sxhkd nnn maim youtube-dl rsync ripgrep bat xz groff upx jq glow translate-shell ctags anki zeal asciinema texlive tinycron tig tree gdb unclutter urlscan urlview uftp
 
 echo "installing audio tools..."
 doas xbps-install -Sy alsa-utils pulseaudio alsa-plugins-pulseaudio pamixer pulsemixer
 
 echo "installing multimedia tools..."
-doas xbps-install -Sy ffmpeg ImageMagick mpv mpd sxiv zathura zathura-pdf-mupdf wkhtmltopdf obs openshot shotcut darktable darktable-cli audacity lmms
+doas xbps-install -Sy ffmpeg ImageMagick mpv mpd sxiv zathura zathura-pdf-mupdf wkhtmltopdf obs openshot shotcut darktable darktable-cli audacity lmms transmission transmission-remote
 
 echo "installing system monitoring tools..."
 doas xbps-install -Sy htop iotop
 
 echo "installing email tools..."
-doas xbps-install -Sy neomutt msmtp
+doas xbps-install -Sy neomutt msmtp thunderbird
 
-echo "installing irc tools..."
-doas xbps-install -Sy irssi weechat weechat-python
+echo "installing chatting applications..."
+doas xbps-install -Sy irssi weechat weechat-python Signal-Desktop telegram-desktop cordless 6cord tuir toxcore toxic utox
 
 echo "installing web browsers..."
 doas xbps-install -Sy chromium firefox lynx w3m tor torbrowser-launcher
@@ -49,17 +49,11 @@ echo "installing fonts..."
 doas xbps-install -Sy font-ibm-plex-otf font-inconsolata-otf
 
 echo "installing networking stuff..."
-doas xbps-install -Sy nmap netcat wireshark inetutils net-tools iperf3 iputils aircrack-ng arp-scan
+doas xbps-install -Sy ufw nmap netcat wireshark inetutils net-tools iperf3 iputils aircrack-ng arp-scan
 
 echo "installing programming stuff..."
-doas xbps-install -Sy rustup go python3 nodejs yarn R ruby grpc protobuf cloc scc
-
-echo "installing linters..."
-doas xbps-install -Sy shellcheck pylint vale tflint
+doas xbps-install -Sy rustup go python3 nodejs yarn R ruby grpc protobuf apl cloc scc android-tools terraform docker docker-compose docker-credential-pass shellcheck pylint vale tflint
 pip install vint
-
-echo "installing docker..."
-doas xbps-install -Sy docker docker-compose docker-credential-pass
 
 
 prgdir="$HOME/programs"
@@ -93,8 +87,19 @@ doas make install
 
 cd "$prgdir"
 
+echo "installing telegram cli..."
+[ ! -e "$prgdir/tg" ] && git clone "git@github.com:kenorb-contrib/tg.git"
+cd "$prgdir/tg"
+git checkout master
+doas make install
+
+cd "$prgdir"
+
 echo "installing z..."
 [ ! -e "$prgdir/z" ] && git clone "git@github.com:rupa/z.git"
 
 echo "installing v..."
 [ ! -e "$prgdir/v" ] && git clone "git@github.com:rupa/v.git"
+
+echo "installing SETools..."
+[ ! -e "$prgdir/setools" ] && git clone "git@github.com:SELinuxProject/setools.git"
