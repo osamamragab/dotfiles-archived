f541dab26614f09129db37598a16536185d93466
Osama <iosamaify@gmail.com>
2021-01-17 06:39:41 +0200

    sysinit: execute setup script

--- sysinit.sh
+++ sysinit.sh
@@ -111,3 +111,7 @@ echo "installing v..."
 
 echo "installing SETools..."
 [ ! -e "$prgdir/setools" ] && git clone "git@github.com:SELinuxProject/setools.git"
+
+[ -x "$HOME/dotfiles/setup.sh" ] && exec "$HOME/dotfiles/setup.sh"
+
+echo "done"
