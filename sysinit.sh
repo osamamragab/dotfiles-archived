f7ace33c5837c752a2a7b31cef654680ae0f41e1
Osama <iosamaify@gmail.com>
2021-03-13 09:57:46 +0200

    sysinit: add gnupg

--- sysinit.sh
+++ sysinit.sh
@@ -32,7 +32,7 @@ echo "installing x11 tools..."
 i xdotool xclip xautolock xzoom
 
 echo "installing general tools..."
-i curl wget gnupg2 openssh pass passmenu sxhkd nnn maim dunst xdg-utils entr nq youtube-dl rsync exa ripgrep fd skim fzf bat xz translate-shell scron cronie libnotify tree unclutter urlscan urlview uftp highlight redshift newsboat spt sc-im surfraw thttpd miniserve youtube-viewer mps-youtube stagit hyperfine diskus pam-gnupg abook notmuch isync msmtp xwallpaper
+i curl wget gnupg gnupg2 openssh pass passmenu sxhkd nnn maim dunst xdg-utils entr nq youtube-dl rsync exa ripgrep fd skim fzf bat xz translate-shell scron cronie libnotify tree unclutter urlscan urlview uftp highlight redshift newsboat spt sc-im surfraw thttpd miniserve youtube-viewer mps-youtube stagit hyperfine diskus pam-gnupg abook notmuch isync msmtp xwallpaper
 
 echo "installing manual pages..."
 i man-pages man-pages-devel man-pages-posix cheat tldr
