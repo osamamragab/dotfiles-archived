9d4bdb9f9987df8ead0890c0437823799270be4c
Osama <iosamaify@gmail.com>
2021-06-01 18:11:12 +0200

    setup: symlink .editorconfig

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cdir="$(dirname "$(readlink -f "$0")")"
-cp -rsv "$cdir/.config" "$cdir/.local" "$cdir/.zprofile" "$HOME"
+cp -rsv "$cdir/.config" "$cdir/.local" "$cdir/.zprofile" "$cdir/.editorconfig" "$HOME"
 ln -s "$HOME/.config/tmux/tmux.conf" "$HOME/.tmux.conf"
