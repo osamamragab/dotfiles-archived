81db2c25d4b0d9768cb6654027ec8e6068350c2f
Osama <iosamaify@gmail.com>
2021-07-20 18:12:10 +0200

    setup: force copy

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cdir="$(dirname "$(readlink -f "$0")")"
-cp -rsv "$cdir/.config" "$cdir/.local" "$cdir/.zprofile" "$cdir/.editorconfig" "$HOME"
-ln -s "$HOME/.config/tmux/tmux.conf" "$HOME/.tmux.conf"
+cp -srfv "$cdir/.config" "$cdir/.local" "$cdir/.zprofile" "$cdir/.editorconfig" "$HOME"
+ln -sf "$HOME/.config/tmux/tmux.conf" "$HOME/.tmux.conf"
