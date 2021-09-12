efce9bf9d2d8f88a8068fe890de4987ad7465c30
Osama <iosamaify@gmail.com>
2021-09-12 06:28:43 +0200

    sysinit: add xtools

--- sysinit.sh
+++ sysinit.sh
@@ -54,7 +54,7 @@ xi "build tools" \
 	gcc git make pkg-config
 
 xi "general tools" \
-	curl wget gnupg gnupg2 openssh pass passmenu tmux nnn slop maim dunst xdg-utils time entr vsv mlocate fzf ripgrep cronie libnotify xcompmgr unclutter urlview darkhttpd youtube-dl pam-gnupg xdotool xclip yank xwallpaper pmount autofs tar bsdtar xz zip unzip tlp delta tig exiftool \
+	curl wget gnupg gnupg2 openssh pass passmenu tmux nnn slop maim dunst xdg-utils xtools time entr vsv mlocate fzf ripgrep cronie libnotify xcompmgr unclutter urlview darkhttpd youtube-dl pam-gnupg xdotool xclip yank xwallpaper pmount autofs tar bsdtar xz zip unzip tlp delta tig exiftool \
 	- lshw rsync earlyoom spt sc-im newsboat surfraw android-tools scrcpy translate-shell xautolock miniserve youtube-viewer openntpd bluez lprng qemu lftp redshift fribidi arandr tree bat glow dragon catimg \
 	-- mdp screenkey github-cli glab
 
