fa43748c3a9e719f4de4350cfeb29c5494fab466
Osama <iosamaify@gmail.com>
2021-11-05 14:55:09 +0200

    sysinit: add zbar & qrencode

--- sysinit.sh
+++ sysinit.sh
@@ -56,7 +56,7 @@ xi "build tools" \
 
 xi "general tools" \
 	curl wget gnupg gnupg2 openssh pass passmenu keepassxc tmux nnn slop maim dunst xdg-utils xtools time entr vsv mlocate fzf ripgrep cronie libnotify xcompmgr unclutter urlview darkhttpd youtube-dl pam-gnupg xdotool xclip yank xwallpaper pmount autofs tar bsdtar xz zip unzip tlp delta tig exiftool redshift newsboat \
-	- lshw rsync inotify-tools earlyoom spt sc-im surfraw android-tools scrcpy translate-shell xautolock miniserve yt-dlp youtube-viewer aria2 openntpd bluez lprng qemu lftp fribidi arandr tree bat glow dragon catimg mdp screenkey git-extras git-lfs xoris fbida buku \
+	- lshw rsync inotify-tools earlyoom spt sc-im surfraw android-tools scrcpy translate-shell xautolock miniserve yt-dlp youtube-viewer aria2 openntpd bluez lprng qemu lftp fribidi arandr tree bat glow dragon catimg mdp screenkey git-extras git-lfs xoris fbida buku zbar qrencode \
 	-- glab github-cli pcmanfm xarchiver ffmpegthumbnailer gnome-epub-thumbnailer
 
 xi "manual pages" \
