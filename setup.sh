e61a2635a7b4bf1a2628963a1c71a232131b5541
Osama <iosamaify@gmail.com>
2021-11-11 00:31:08 +0200

    setup: symlinks stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cdir="$(dirname "$(readlink -f "$0")")"
-cp -srfv "$cdir/.config" "$cdir/.local" "$cdir/.zprofile" "$cdir/.editorconfig" "$HOME"
+cp -srfv "$cdir/.config" "$cdir/.local" "$HOME"
+ln -sf "$HOME/.config/shell/profile" "$HOME/.zprofile"
 ln -sf "$HOME/.config/tmux/tmux.conf" "$HOME/.tmux.conf"
